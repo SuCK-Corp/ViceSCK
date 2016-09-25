e846c39ea5c2ab80f83d91198c42e67212974504
Chris Rohr <chrisdrohr@gmail.com>
2016-09-25 13:44:49 -0700

    adds 360 rotation on category fab

--- ViceNews/app/src/main/java/com/example/bboyc/vicenews/MainActivity.java
+++ ViceNews/app/src/main/java/com/example/bboyc/vicenews/MainActivity.java
@@ -2,9 +2,9 @@ package com.example.bboyc.vicenews;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -17,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
     Animation fab_close, fab_open, rotate_anticlockwise, rotate_clockwise;
     boolean isOpen = true;
+
+
+
 
 
     @Override
@@ -130,16 +133,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
 
                 return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        return super.onOptionsItemSelected(item);
+    public boolean collapseFab (SearchView searchView) {
+        searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fab.callOnClick();
+            }
+        });
+        return true;
     }
+
 }
